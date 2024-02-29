bb414fe8d3bfa87737a41d67b07d5b610aedef18
jmanwel <new.juang@gmail.com>
2024-02-29 08:26:03 -0300

    [MOD] index.js

--- index.js
+++ index.js
@@ -47,16 +47,59 @@ let playerOMoves = [
     [0,0,0]
 ];
 
+const RUNNING = "RUNIING";
+const PLAYER_X_WINS = "PLAYER_X_WINS";
+const PLAYER_O_WINS = "PLAYER_O_WINS";
+const CATS_GAME = "CATS_GAME";
+
+function getNextGameState(xMoves, oMoves){
+    let playerXWins = isHorizontalWin (xMoves);
+    let playerOWins = isHorizontalWin (oMoves);
+
+    if (playerXWins){
+        return PLAYER_X_WINS;
+    }
+
+    if (playerOWins) {
+        return PLAYER_O_WINS;
+    }
+
+    return RUNNING;
+}
+
+function isHorizontalWin (moves){
+    return moves.some(row => row.every(x => x === 1))
+}
+
+function isVerticalWin (moves){}
+function isCatsGame (moves){}
 
 async function startGame() {
+    let currentGameState = RUNNING;
+    drawGrid(playerXMoves, playerOMoves);
     while (!game_is_over){
-        drawGrid(playerXMoves, playerOMoves);
         let response = await displayPrompt(`${currentPlayer}, please enter your next move: `);
         let [yMove, xMove] = response.split(",").map(x=>Number(x));
         let currentPlayerMoves = currentPlayer === "Player X" ? playerXMoves : playerOMoves;
         
         currentPlayerMoves[yMove][xMove]=1;
         currentPlayer = currentPlayer === "Player X" ? "Player O" : "Player X"
+        currentGameState = getNextGameState(playerXMoves, playerOMoves);
+        game_is_over = [PLAYER_X_WINS, PLAYER_O_WINS, CATS_GAME].includes(currentGameState);
+
+        drawGrid(playerXMoves, playerOMoves);
+    }
+
+    if (currentGameState === PLAYER_X_WINS){
+        console.log("Player X is the winne!");
+    }
+
+    if (currentGameState === PLAYER_O_WINS){
+        console.log("Player O is the winner!");
+    }
+
+    if (currentGameState === CATS_GAME){
+        console.log("It's a tie!");
     }
 }
 
